9bfc1abd8e8e6613e8e61a6289ce6c534be0e49c
Ammar Abuelgasim <aabuelgasim@effcomm.com>
2017-09-26 18:13:21 +0200

    Enabling event publishers to publish events for multiple upstream event queues

--- config.go
+++ config.go
@@ -1,31 +1,30 @@
 package evently
 
-import "github.com/RichardKnop/machinery/v1/config"
+import (
+	"strings"
 
-// ServerConfig is a wrapper around the machinery config type
-type ServerConfig *config.Config
+	"github.com/RichardKnop/machinery/v1/config"
+)
+
+// Configuration is a wrapper around the machinery config type
+type Configuration struct {
+	ServerConfig   *config.Config
+	UpstreamQueues []string
+}
 
 // GetConfiguration retrieves the event server configuration
-func GetConfiguration(settings map[string]string) ServerConfig {
-	var configuration ServerConfig
-
-	switch settings["source"] {
-	case "file":
-		configuration = config.NewFromYaml(settings["path"], true, true)
-	case "environment":
-		configuration = config.NewFromEnvironment(true, true)
-	case "manual":
-		configuration = &config.Config{
+func GetConfiguration(settings map[string]string) Configuration {
+	return Configuration{
+		ServerConfig: &config.Config{
 			Broker:        settings["broker"],
-			DefaultQueue:  settings["default_queue"],
+			DefaultQueue:  settings["downstream_queue"],
 			ResultBackend: settings["result_backend"],
 			AMQP: &config.AMQPConfig{
 				Exchange:     settings["amqp_exchange"],
 				ExchangeType: settings["amqp_exchange_type"],
 				BindingKey:   settings["amqp_binding_key"],
 			},
-		}
+		},
+		UpstreamQueues: strings.Split(settings["upstream_queues"], ";"),
 	}
-
-	return configuration
 }
